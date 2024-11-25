17c5477fc3420f6a874e1f55d0d01b84b98c6192
joselvrg <jpedraza16@alumnos.uaq.mx>
2024-11-24 23:55:47 -0600

    fotos y firmas
    crea una carpeta llamada uploads y ahi se van a guardar las fotos y firmas

--- ILicencias.php
+++ ILicencias.php
@@ -1,37 +1,43 @@
 <?php
-    $NoLicencia=$_REQUEST['NoLicencia'];
-    $Foto=$_REQUEST['Foto'];
-    $Nombre=$_REQUEST['Nombre'];
-    $Apellido=$_REQUEST['Apellido'];
-    $Firma=$_REQUEST['Firma'];
-    $TipoLicencia=$_REQUEST['TipoLicencia'];
-    $FechaExp=$_REQUEST['FechaExp']; 
-    $Observacion=$_REQUEST['Observacion'];
-    $Antiguedad =$_REQUEST['Antiguedad'];
-    $Domicilio =$_REQUEST['Domicilio'];
-    $Restriccion =$_REQUEST['Restriccion'];
-    $Vigencia =$_REQUEST['Vigencia'];
-    $Conductor =$_REQUEST['Conductor'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    include("Controlador.php");
 
+    $NoLicencia = $_POST['NoLicencia'];
+    $Foto = $_FILES['Foto']['name'];
+    $Firma = $_FILES['Firma']['name'];
+    $Nombre = $_POST['Nombre'];
+    $Apellido = $_POST['Apellido'];
+    $TipoLicencia = $_POST['TipoLicencia'];
+    $FechaExp = $_POST['FechaExp'];
+    $Observacion = $_POST['Observacion'];
+    $Antiguedad = $_POST['Antiguedad'];
+    $Domicilio = $_POST['Domicilio'];
+    $Restriccion = $_POST['Restriccion'];
+    $Vigencia = $_POST['Vigencia'];
+    $ConductorID = $_POST['ConductorID'];
 
-    $SQL ="INSERT INTO Licencias VALUES('$NoLicencia','$Foto','$Nombre','$Apellido',
-   '$Firma', '$TipoLicencia','$FechaExp','$Observacion',
-    '$Antiguedad','$Domicilio',
-    '$Restriccion','$Vigencia','$Conductor')";
-    /*print($SQL)*/;
+    $target_dir = "uploads/";
+    if (!is_dir($target_dir)) {
+        mkdir($target_dir, 0777, true);
+    }
 
+    $target_file_foto = $target_dir . basename($Foto);
+    $target_file_firma = $target_dir . basename($Firma);
 
-//Enviar datos al controlador 
-include("Controlador.php"); 
-$Con=Conectar();
-$ResultSet=Ejecutar($Con,$SQL);
-if ($ResultSet == 1){
-    print("Registro insertado");
-}else{
-    print("Error"); 
+    if (move_uploaded_file($_FILES["Foto"]["tmp_name"], $target_file_foto) && move_uploaded_file($_FILES["Firma"]["tmp_name"], $target_file_firma)) {
+        $Con = Conectar();
+        $SQL = "INSERT INTO Licencias (NoLicencia, Foto, Firma, Nombre, Apellido, TipoLicencia, FechaExp, Observacion, Antiguedad, Domicilio, Restriccion, Vigencia, ConductorID)
+                VALUES ('$NoLicencia', '$target_file_foto', '$target_file_firma', '$Nombre', '$Apellido', '$TipoLicencia', '$FechaExp', '$Observacion', '$Antiguedad', '$Domicilio', '$Restriccion', '$Vigencia', '$ConductorID')";
+
+        try {
+            $ResultSet = Ejecutar($Con, $SQL);
+            echo "Registro insertado exitosamente.";
+        } catch (mysqli_sql_exception $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        Desconectar($Con);
+    } else {
+        echo "Hubo un error al subir los archivos.";
+    }
 }
-Desconectar($Con); 
-
-
-   
 ?>
